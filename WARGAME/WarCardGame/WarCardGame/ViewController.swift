012773b406267a59ab8776ab403a0822bf6cf963
helloimali <toalisaeedmalik@gmail.com>
2019-05-08 16:09:26 -0400

    Constraints added to smaller devices and connections made from storyboard to ViewController

--- WARGAME/WarCardGame/WarCardGame/ViewController.swift
+++ WARGAME/WarCardGame/WarCardGame/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LeftImgView: UIImageView!
+    
+    @IBOutlet weak var RIghtImgView: UIImageView!
+    
+    
+    @IBOutlet weak var PlayerScore: UILabel!
+    
+    @IBOutlet weak var CPUScore: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func dealTap(_ sender: Any) {
+        print("Deal")
+    }
+    
 }
 
