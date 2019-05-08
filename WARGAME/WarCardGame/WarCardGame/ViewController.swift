c9938f219801f593a075615aee9710f552a1a082
helloimali <toalisaeedmalik@gmail.com>
2019-05-08 14:23:51 -0400

    UI for War Card Game

---
+++ WARGAME/WarCardGame/WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Ali Malik on 5/8/19.
+//  Copyright © 2019 Malik Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
