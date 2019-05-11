3506c48764705f25cfdaaf4f2a7938dfb112b32d
helloimali <toalisaeedmalik@gmail.com>
2019-05-11 19:20:03 -0400

    Installed Cocoapods, edited POD file, installed Alamofire with Cocoapods
    pot init
    pod install

---
+++ CocoaPodsTesting/cocoa/cocoa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cocoa
+//
+//  Created by Ali Malik on 5/11/19.
+//  Copyright © 2019 Malik Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
