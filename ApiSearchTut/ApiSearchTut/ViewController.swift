f2e870bd9b3c65cdd8a2e8e7fc10b8a6636862ff
helloimali <toalisaeedmalik@gmail.com>
2019-05-13 18:37:39 -0400

    New project
    Search with Swifty and Alamo

---
+++ ApiSearchTut/ApiSearchTut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ApiSearchTut
+//
+//  Created by Ali Malik on 5/13/19.
+//  Copyright © 2019 Malik Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ApiSearchTut/Pods/Target Support Files/Pods-ApiSearchTut/Pods-ApiSearchTut-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ApiSearchTut : NSObject
+@end
+@implementation PodsDummy_Pods_ApiSearchTut
+@end
