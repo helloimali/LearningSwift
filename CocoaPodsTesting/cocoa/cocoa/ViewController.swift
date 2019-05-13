33b4faa9fa1390ee271092aa6f56ecb4e53dcc71
helloimali <toalisaeedmalik@gmail.com>
2019-05-13 00:49:18 -0400

    A non functinal post request?
    https://medium.com/@farhansyed/interacting-with-a-backend-in-swift-56491d8ffaa3

--- CocoaPodsTesting/cocoa/cocoa/ViewController.swift
+++ CocoaPodsTesting/cocoa/cocoa/ViewController.swift
@@ -19,19 +19,44 @@ class ViewController: UIViewController {
         
         //Alamofire.request(postsURLEndPoint, method: .get).responseJSON{response in print(response.result.value)}
         
-        Alamofire.request(postsURLEndPoint)
-            .responseJSON{ response in
-                guard let json = response.result.value as? [String:Any] else{
-                    return
-                }
-            print(json)
-            print(json["title"])
-            print("THIS IS THE BODY: ")
-            print(json["body"])
-        }
+//        Alamofire.request(postsURLEndPoint)
+//            .responseJSON{ response in
+//                guard let json = response.result.value as? [String:Any] else{
+//                    return
+//                }
+//            print(json)
+//            print(json["title"])
+//            print("THIS IS THE BODY: ")
+//            print(json["body"])
+//        }
         
         
 
+        let newPost: [String: Any] = ["userId" : 12345, "title": "This is a POST request", "Body": "This reqeust is sent with Alamofire"]
+        
+        Alamofire.request(postsURLEndPoint, method: .post, parameters: newPost,
+                          encoding: JSONEncoding.default)
+            .responseJSON { response in
+                guard response.result.error == nil else {
+                    // got an error in getting the data, need to handle it
+                    print("error")
+                    print(response.result.error!)
+                    return
+                }
+                // unwrap JSON
+                guard let json = response.result.value as? [String: Any] else {
+                    print("No JSON")
+                    // Could not get JSON
+                    return
+                }
+                // use json
+                guard let postTitle = json["title"] as? String else {
+                    // Could not get title from json
+                    print("as")
+                    return
+                }
+                print("Post title: " + postTitle)
+        }
     
     
     }
