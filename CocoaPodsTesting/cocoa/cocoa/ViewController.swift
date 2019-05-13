4ad402bcc9683fda305736e422bf976561bf94b1
helloimali <toalisaeedmalik@gmail.com>
2019-05-12 23:37:52 -0400

    Created Get request with Alamofire
    Using Alamo fire, created a get request, took the received data in JSON, created a dictionary and added the received data into "JSON" variable, then printed entire dictionary, then just the title, then the body

--- CocoaPodsTesting/cocoa/cocoa/ViewController.swift
+++ CocoaPodsTesting/cocoa/cocoa/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let postsURLEndPoint: String = "https://jsonplaceholder.typicode.com/posts/1"
+        
+        //Alamofire.request(postsURLEndPoint, method: .get).responseJSON{response in print(response.result.value)}
+        
+        Alamofire.request(postsURLEndPoint)
+            .responseJSON{ response in
+                guard let json = response.result.value as? [String:Any] else{
+                    return
+                }
+            print(json)
+            print(json["title"])
+            print("THIS IS THE BODY: ")
+            print(json["body"])
+        }
+        
+        
+
+    
+    
     }
-
-
 }
 
